4aac4d182df6a4fdcd529e91993fd26654296502
Abhinav <abhinavp54206@gmail.com>
2024-12-03 23:05:27 +0530

    Add todo.js

---
+++ todo.js
@@ -0,0 +1,11 @@
+// Create an array with 5 tasks
+let todoList = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
+
+// Add a new task to the beginning of the list
+todoList.unshift("New Task");
+
+// Remove the last task from the list
+todoList.pop();
+
+// Log the updated to-do list to the console
+console.log(todoList);
