b4f9cce93ab467040cf37d0a502ec09cd4ba975c
Abhinav <abhinavp54206@gmail.com>
2024-12-03 22:58:07 +0530

    Add temp.js

---
+++ temp.js
@@ -0,0 +1,8 @@
+// Create an array to store the temperatures for 7 days
+let temperatures = [25, 28, 30, 26, 24, 29, 31];
+
+// Access the temperature of the 4th day (index 3)
+temperatures[3] = temperatures[3] + 2; // Increase the 4th day's temperature by 2 degrees
+
+// Log the updated temperatures for all 7 days
+console.log(temperatures);
