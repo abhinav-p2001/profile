e13441f1b3650dda9ba9fa2bf34435d0fb8c6108
Abhinav <abhinavp54206@gmail.com>
2024-12-04 22:46:32 +0530

    Add pattern.js

---
+++ pattern.js
@@ -0,0 +1,13 @@
+function printSlashesPattern(rows) {
+    let pattern = "";
+    for (let i = 0; i < rows; i++) {
+      pattern += "\\/"; // Add \/ for each row
+    }
+    console.log(pattern);
+  }
+  
+  // Example usage:
+  printSlashesPattern(2);
+  
+  
+  
