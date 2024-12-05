2c426870cf66253fa4be066bb75152014cced732
Abhinav <abhinavp54206@gmail.com>
2024-12-05 22:13:40 +0530

    Add eligiblity.js

---
+++ eligiblity.js
@@ -0,0 +1,7 @@
+let age = 65; // You can change this value to test with different ages
+
+let result = (age <= 0) ? "Invalid Age" :
+             (age >= 60) ? "Eligible for Senior Discount" :
+             "Not Eligible for Senior Discount";
+
+console.log(result);
