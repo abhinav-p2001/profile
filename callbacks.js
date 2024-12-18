cd7b8ba5fb316b48cad06c73bb63175209c140c2
Abhinav <abhinavp54206@gmail.com>
2024-12-18 21:52:42 +0530

    Add callbacks.js

---
+++ callbacks.js
@@ -0,0 +1,39 @@
+// Define each task function
+function task1(callback) {
+    setTimeout(() => {
+        console.log("Task 1 complete");
+        callback(); // Start the next task
+    }, 1000);
+}
+
+function task2(callback) {
+    setTimeout(() => {
+        console.log("Task 2 complete");
+        callback();
+    }, 1000);
+}
+
+function task3(callback) {
+    setTimeout(() => {
+        console.log("Task 3 complete");
+        callback();
+    }, 1000);
+}
+
+function task4(callback) {
+    setTimeout(() => {
+        console.log("Task 4 complete");
+        callback();
+    }, 1000);
+}
+
+// Execute tasks in order using nested callbacks
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => {
+                console.log("All tasks completed");
+            });
+        });
+    });
+});
